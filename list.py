73565eea5ee904df8e274caa90f9112139344325
Savitha <savitha@email.com>
2021-04-26 06:38:38 +0400

    fourth commit

--- list.py
+++ list.py
@@ -10,5 +10,8 @@
 # print(l)
 l1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 l2 = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-l3 = l1+l2
-print(l3)
+l3 = []
+# l1.extend(l2)
+# print(l1)
+for x in l2: l1.append(x)
+print(l1)
