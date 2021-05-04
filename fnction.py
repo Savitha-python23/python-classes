f39127907aa75bcc837b6045347443d7da56ab53
Savitha <savitha@email.com>
2021-05-04 07:05:45 +0400

    twelve

--- fnction.py
+++ fnction.py
@@ -1,48 +1,64 @@
-def hyper():
-	print("HELLO")
+# def hyper():
+	# print("HELLO")
 # hyper()
 
 
 # Single return
 
-def up():
-	s = "SAVITHA"
-	return s[::-1]
+# def up():
+	# s = "SAVITHA"
+	# return s[::-1]
 # print(up())
 
-def d2():
-	data = up()
-	return data
+# def d2():
+	# data = up()
+	# return data
 
-print(d2())
+# print(d2())
 
 #-------------------------------------------------------
 
 # Multiple return
 
-def evvi():
-	say = "Hello"
-	bye = "bye"
-	return say, bye
+# def evvi():
+	# say = "Hello"
+	# bye = "bye"
+	# return say, bye
 
-def call():
-	s, v = evvi()
-	return s, v
-print(call())
+# def call():
+	# s, v = evvi()
+	# return s, v
+# print(call())
 
 # ----------------------------------------------------------------
 
-def do_something(name):
-	return "Hello", name
-print(do_something(1))
+# def do_something(name):
+	# return "Hello", name
+# print(do_something(1))
 
 #-------------------------------------------------
 
-def jio():
-	s = {1, 2, 3}
-	u = {1, 3, 4}
+def jio(s, u):   # Method 1
 	return(s.intersection(u))
-print(jio())
+print(jio({1, 2, 3}, {1, 3, 4}))
+#------------------------------------------
+
+# def sara(name):
+	# k = "Savitha"
+	# return k,name
+# def function():
+	# d = sara("Kavi")
+	# return(d)
+# print(function())
+
+#--------------------------------------------
+
+
+
+
+
+
+
 
 
 
