a61fd15e70e88f796996a8efb0163063380cf1ce
Savitha <savitha@email.com>
2021-05-06 07:31:48 +0400

    thirteen

--- multifunday4.py
+++ multifunday4.py
@@ -2,13 +2,24 @@ def mul(num, mul):
 	for data in range(1, num + 1):  # Method 1
 		print(data * mul)
 mul(10,5)
+#----------------------------------------------
 
 def mul(num, m):
 	value = []
-	for data in range(1, num + 1):
+	for data in range(1, num + 1): # Method 2 using append method
 		value.append(data * m)
 	return value
 print(mul(10,5))
+
+#----------------------------------------------
+#List compression 
+
+def mult(n, s):
+	data = [d * s for d in range(1, n + 1)] #  simple method for multiplication
+	return data 
+print(mult(10,5))
+
+#-----------------------------------------------
 
 
 	
