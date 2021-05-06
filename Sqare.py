49b8a4f2017656cbc626b4490bf7261dee51cdc0
Savitha <savitha@email.com>
2021-05-06 07:31:48 +0400

    thirteen

--- Sqare.py
+++ Sqare.py
@@ -1,3 +1,19 @@
-l = [1, 2, 3, 4]
-square = [number ** 2 for number in l]
-print(square)
+def sqr(square):
+	l = []
+	for data in square:
+		l.append(data * data)#--- Method 1
+	print(l)
+sqr([1,2,3,4])
+#-------------------------------
+
+def sqre(a):
+	data = [d * d for d in a] # ----- list comprehension method 2
+	print(data)
+sqre([1,2,3,4])
+
+#--------------------------------------------
+
+def sqt(element):
+	return element * element
+data = list(map(sqt, [1,2,3,4])) #-------- method 3 using map
+print(data)
